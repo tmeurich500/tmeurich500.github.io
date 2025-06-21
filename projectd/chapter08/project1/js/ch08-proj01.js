4d5464c146b401f013ead115a2f4116f9bb33f93
tmeurich500 <tmeurich500@gmail.com>
2025-06-20 20:59:16 -0400

    final fix to make calculations work when hosted on a non local server

--- projectd/chapter08/project1/js/ch08-proj01.js
+++ projectd/chapter08/project1/js/ch08-proj01.js
@@ -1,34 +1,40 @@
-const tax_rate = prompt('Enter tax rate (e.g. 0.10)');
-const shipping_threshold = prompt('Enter shipping threshold (e.g. 1000)');
 
-const tableBody = document.querySelector("tbody");
-let html = "";
+window.onload = function() {
+    document.querySelector("button").addEventListener("click", calculateCart);
+};
 
-for (let item of cart) {
-    const { title, filename, price } = item.product;
-    const quantity = item.quantity;
-    const amount = price * quantity;
+function calculateCart() {
+    const tax_rate = parseFloat(document.getElementById("taxRate").value);
+    const shipping_threshold = parseFloat(document.getElementById("shipThreshold").value);
 
-    html += `
-        <tr>
-            <td><img src="images/${filename}"></td>
-            <td>${title}</td>
-            <td>${quantity}</td>
-            <td>$${price.toFixed(2)}</td>
-            <td>$${amount.toFixed(2)}</td>
-        </tr>
-    `;
+    const tableBody = document.getElementById("cart-body");
+    let html = "";
+
+    for (let item of cart) {
+        const { title, filename, price } = item.product;
+        const quantity = item.quantity;
+        const amount = price * quantity;
+
+        html += `
+            <tr>
+                <td><img src="images/${filename}"></td>
+                <td>${title}</td>
+                <td>${quantity}</td>
+                <td>$${price.toFixed(2)}</td>
+                <td>$${amount.toFixed(2)}</td>
+            </tr>
+        `;
+    }
+
+    tableBody.innerHTML = html;
+
+    const subtotal = calculateSubtotal(cart);
+    const tax = calculateTax(subtotal, tax_rate);
+    const shipping = calculateShipping(subtotal, shipping_threshold);
+    const grandTotal = calculateGrandTotal(subtotal, tax, shipping);
+
+    document.getElementById("subtotal").textContent = `$${subtotal.toFixed(2)}`;
+    document.getElementById("tax").textContent = `$${tax.toFixed(2)}`;
+    document.getElementById("shipping").textContent = `$${shipping.toFixed(2)}`;
+    document.getElementById("grandtotal").textContent = `$${grandTotal.toFixed(2)}`;
 }
-
-tableBody.innerHTML = html;
-
-const subtotal = calculateSubtotal(cart);
-const tax = calculateTax(subtotal, tax_rate);
-const shipping = calculateShipping(subtotal, shipping_threshold);
-const grandTotal = calculateGrandTotal(subtotal, tax, shipping);
-
-// Insert totals
-document.getElementById("subtotal").textContent = `$${subtotal.toFixed(2)}`;
-document.getElementById("tax").textContent = `$${tax.toFixed(2)}`;
-document.getElementById("shipping").textContent = `$${shipping.toFixed(2)}`;
-document.getElementById("grandtotal").textContent = `$${grandTotal.toFixed(2)}`;
